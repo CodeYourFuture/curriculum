e1287474793b5be0ac8c8fe513a49b41272f949c
Sally McGrath <sally@codeyourfuture.io>
2023-06-26 15:58:28 +0200

    revise sw
    - move to stale while revalidate cache strategy
    - rm unused image cache limit
    - skip caching external images to reduce CORS problems
    - fix cloney clone error
    - everything else largely the same

--- static/sw.js
+++ static/sw.js
@@ -3,8 +3,6 @@ const IMAGE_CACHE_CYF = "cyf-image-cache";
 const FONT_CACHE_CYF = "cyf-font-cache";
 const urlsToCache = ["/"];
 
-const IMAGE_CACHE_CYF_LIMIT = 50;
-
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_CYF).then(function (cache) {
@@ -16,34 +14,53 @@ self.addEventListener("install", function (event) {
 self.addEventListener("fetch", function (event) {
   const requestUrl = new URL(event.request.url);
 
+  // Ignore non-HTTP/HTTPS requests
+  if (!requestUrl.protocol.startsWith("http")) {
+    return;
+  }
+
+  // Bypass cache for external image requests
+  if (
+    requestUrl.origin !== location.origin &&
+    /\.(jpe?g|png|webp)$/.test(requestUrl.pathname)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
-    // Try to fetch from network first
-    fetch(event.request)
-      .then(function (networkResponse) {
-        // If request is successful, clone and cache the response
-        let cacheName = requestUrl.pathname;
+    caches.match(event.request).then(function (cachedResponse) {
+      var fetchPromise = fetch(event.request)
+        .then(function (networkResponse) {
+          if (!networkResponse || networkResponse.status !== 200) {
+            return cachedResponse;
+          }
 
-        // Choose the cache depending on the file type
-        if (/\.woff2?$|\.ttf$/.test(requestUrl.pathname)) {
-          cacheName = FONT_CACHE_CYF;
-        } else if (/\.jpe?g$|\.png$|\.webp$/.test(requestUrl.pathname)) {
-          cacheName = IMAGE_CACHE_CYF;
-        } else {
-          cacheName = CACHE_CYF;
-        }
+          let cacheName = CACHE_CYF;
+          let responseToCache = networkResponse.clone();
 
-        caches.open(cacheName).then(function (cache) {
-          cache.put(event.request, networkResponse.clone());
+          // Choose the cache depending on the file type
+          if (/\.woff2?$|\.ttf$/.test(requestUrl.pathname)) {
+            cacheName = FONT_CACHE_CYF;
+          } else if (/\.jpe?g$|\.png$|\.webp$/.test(requestUrl.pathname)) {
+            cacheName = IMAGE_CACHE_CYF;
+          }
+
+          caches.open(cacheName).then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return networkResponse;
+        })
+        .catch(function (error) {
+          console.log(
+            "[Service Worker] Network request Failed. Serving content from cache: " +
+              error
+          );
+          return cachedResponse;
         });
 
-        // Return the network response
-        return networkResponse;
-      })
-      .catch(function () {
-        // If network request fails, try to get from cache
-        return caches.match(event.request).then(function (cacheResponse) {
-          return cacheResponse;
-        });
-      })
+      return fetchPromise || cachedResponse;
+    })
   );
 });
