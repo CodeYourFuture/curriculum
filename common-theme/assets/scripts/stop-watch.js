8377dd35c16afc622ed25ee7b812638875d0266d
Sally McGrath <sally@codeyourfuture.io>
2024-06-26 09:41:36 +0100

    add kbd activation
    someone is on the wrong linter here...

--- common-theme/assets/scripts/stop-watch.js
+++ common-theme/assets/scripts/stop-watch.js
@@ -1,105 +1,116 @@
 class StopWatch {
-    constructor() {
-        if (StopWatch.instance) {
-            return StopWatch.instance;
+  constructor() {
+    if (StopWatch.instance) {
+      return StopWatch.instance;
+    }
+    this.attachListener();
+    StopWatch.instance = this;
+  }
+
+  attachListener() {
+    const timeElements = document.querySelectorAll(".c-block__time");
+
+    timeElements.forEach((timeElement) => {
+      timeElement.addEventListener("keydown", (event) => {
+        if (
+          event.key === "Enter" &&
+          !timeElement.classList.contains("is-active")
+        ) {
+          this.startTimer(timeElement);
         }
-        this.attachListener();
-        StopWatch.instance = this;
-    }
-
-    attachListener() {
-        const timeElements = document.querySelectorAll('.c-block__time');
-
-        timeElements.forEach(timeElement => {
-            timeElement.addEventListener('click', () => {
-                if (!timeElement.classList.contains('is-active')) {
-                    this.startTimer(timeElement);
-                }
-            });
-        });
-    }
-
-    startTimer(timeElement) {
-        const datetime = timeElement.getAttribute('datetime');
-        const duration = this.parseDuration(datetime);
-
-        if (!this.isValidDuration(duration)) {
-            console.error("Invalid duration specified in datetime attribute.");
-            return;
+      });
+      timeElement.addEventListener("click", () => {
+        if (!timeElement.classList.contains("is-active")) {
+          this.startTimer(timeElement);
         }
+      });
+    });
+  }
 
-        let startTime = Date.now();
-        timeElement.classList.add('is-active');
+  startTimer(timeElement) {
+    const datetime = timeElement.getAttribute("datetime");
+    const duration = this.parseDuration(datetime);
 
-        const timerInterval = setInterval(() => {
-            const remainingTime = this.updateTime(timeElement, startTime, duration);
-
-            if (remainingTime <= 0) {
-                clearInterval(timerInterval);
-                timeElement.classList.remove('is-active');
-                timeElement.setAttribute('aria-live', 'assertive');
-                this.playAlarm(2, 200, 200);
-            }
-        }, 1000);
+    if (!this.isValidDuration(duration)) {
+      console.error("The datetime attribute is not valid. Please fix it.");
+      return;
     }
 
-    playAlarm(repetitions, delayBetweenRepetitions, delayBetweenSounds) {
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const audioCtx = new AudioContext();
+    let startTime = Date.now();
+    timeElement.classList.add("is-active");
 
-        const playSound = async () => {
-            const oscillator = audioCtx.createOscillator();
-            oscillator.type = 'ine';
-            oscillator.frequency.setValueAtTime(1000, audioCtx.currentTime);
-            oscillator.connect(audioCtx.destination);
-            oscillator.start();
+    const timerInterval = setInterval(() => {
+      const remainingTime = this.updateTime(timeElement, startTime, duration);
 
-            await new Promise(resolve => setTimeout(resolve, delayBetweenSounds));
-            oscillator.stop();
-        };
+      if (remainingTime <= 0) {
+        clearInterval(timerInterval);
+        timeElement.classList.remove("is-active");
+        this.playAlarm(2, 200, 200);
+      }
+    }, 1000);
+  }
 
-        const playNextRepetition = async (repetitionIndex) => {
-            if (repetitionIndex <= repetitions) {
-                await playSound();
-                await new Promise(resolve => setTimeout(resolve, delayBetweenRepetitions));
-                await playNextRepetition(repetitionIndex + 1);
-            } else {
-                audioCtx.close();
-            }
-        };
+  playAlarm(repetitions, delayBetweenRepetitions, delayBetweenSounds) {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioCtx = new AudioContext();
 
-        playNextRepetition(0);
+    const playSound = async () => {
+      const oscillator = audioCtx.createOscillator();
+      oscillator.type = "triangle";
+      oscillator.frequency.setValueAtTime(1000, audioCtx.currentTime);
+      oscillator.connect(audioCtx.destination);
+      oscillator.start();
+
+      await new Promise((resolve) => setTimeout(resolve, delayBetweenSounds));
+      oscillator.stop();
+    };
+
+    const playNextRepetition = async (repetitionIndex) => {
+      if (repetitionIndex <= repetitions) {
+        await playSound();
+        await new Promise((resolve) =>
+          setTimeout(resolve, delayBetweenRepetitions)
+        );
+        await playNextRepetition(repetitionIndex + 1);
+      } else {
+        audioCtx.close();
+      }
+    };
+
+    playNextRepetition(0);
+  }
+
+  parseDuration(datetime) {
+    const match = datetime.match(/P(\d+)M/);
+    if (!match) {
+      return NaN;
+    }
+    const minutes = parseInt(match[1]);
+    return minutes * 60 * 1000; // Convert minutes to milliseconds
+  }
+
+  isValidDuration(duration) {
+    return !isNaN(duration) && duration > 0;
+  }
+
+  updateTime(element, startTime, duration) {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - startTime;
+    const remainingTime = duration - elapsedTime;
+
+    if (remainingTime <= 0) {
+      element.textContent = "Countdown finished!";
+      return 0;
     }
 
-    parseDuration(datetime) {
-        const match = datetime.match(/P(\d+)M/);
-        if (!match) {
-            return NaN;
-        }
-        const minutes = parseInt(match[1]);
-        return minutes * 60 * 1000; // Convert minutes to milliseconds
-    }
+    const minutes = Math.floor(remainingTime / 60000);
+    const seconds = Math.floor((remainingTime % 60000) / 1000);
 
-    isValidDuration(duration) {
-        return!isNaN(duration) && duration > 0;
-    }
-
-    updateTime(element, startTime, duration) {
-        const currentTime = Date.now();
-        const elapsedTime = currentTime - startTime;
-        const remainingTime = duration - elapsedTime;
-
-        if (remainingTime <= 0) {
-            element.textContent = "Countdown finished!";
-            return 0;
-        }
-
-        const minutes = Math.floor(remainingTime / 60000);
-        const seconds = Math.floor((remainingTime % 60000) / 1000);
-
-        element.textContent = `${minutes} minutes ${seconds < 10? '0' : ''}${seconds} seconds ⏱`;
-        return remainingTime;
-    }
+    element.textContent = `${minutes} minutes ${
+      seconds < 10 ? "0" : ""
+    }${seconds} seconds ⏱`;
+    return remainingTime;
+  }
 }
 
 const stopwatchInstance = new StopWatch();
