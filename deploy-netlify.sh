2ecc3cac37f638ef2b7f4cf94057dd330ecbfe8e
Daniel Wagner-Hall <dawagner@gmail.com>
2024-07-04 15:42:15 -0400

    Add netlify deploy script
    We were previously not setting --baseURL which means that deploy
    previews have internal links to the prod deploys rather than the deploy
    previews.
    
    Instead we introduce a script so that we can edit for all of the sites
    without needing to go change several site configs each time we want to
    make a change.

---
+++ deploy-netlify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# We set --baseURL on netlify so that any references to .Permalink end up pointing at deploy preview pages, rather than being hard-coded to point at the production URLs.
+hugo --minify --baseURL $DEPLOY_PRIME_URL && npx pagefind --site "public"
