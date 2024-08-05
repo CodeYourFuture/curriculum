c9066f6145147cf37798065054c351c97f8d2abb
Sally McGrath <sally@codeyourfuture.io>
2024-08-05 15:51:22 +0100

    netlify help is now saying this

--- org-cyf-launch/stackbit.config.ts
+++ org-cyf-launch/stackbit.config.ts
@@ -1,23 +1,19 @@
-import { defineStackbitConfig } from "@stackbit/types";
-import { GitContentSource } from "@stackbit/cms-git";
-
 export default defineStackbitConfig({
-  stackbitVersion: "^0.6.0",
+  stackbitVersion: "~0.6.0",
   ssgName: "hugo",
-  nodeVersion: "20",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
       contentDirs: ["content"],
       models: [
         {
-          name: "Home",
           type: "page",
-          urlPath: "/",
-          filePath: "content/_index.md",
-          fields: [{ name: "title", type: "string", required: true }],
+          label: "Home",
+          file: "_index.md",
+          fields: [{ name: "description", type: "string", required: false }],
         },
       ],
     }),
   ],
+  // other properties...
 });
