e93a3d314559bd3a76ca9f1b518d3352943c4373
Sally McGrath <sally@codeyourfuture.io>
2024-08-05 20:46:40 +0100

    resetting up decap
    it seems to want both? I don't really understand this

--- org-cyf-launch/stackbit.config.ts
+++ org-cyf-launch/stackbit.config.ts
@@ -1,19 +1,57 @@
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
-  ssgName: "hugo",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
       contentDirs: ["content"],
       models: [
         {
+          name: "homePage",
           type: "page",
-          label: "Home",
-          file: "_index.md",
-          fields: [{ name: "description", type: "string", required: false }],
+          urlPath: "/",
+          filePath: "content/_index.md",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "description", type: "string" },
+            { name: "layout", type: "string" },
+            { name: "emoji", type: "string" },
+            { name: "map", type: "list" },
+            { name: "weight", type: "number" },
+          ],
+        },
+        {
+          name: "sprint1Prep",
+          type: "page",
+          urlPath: "/sprints/1/prep/",
+          filePath: "sprints/1/prep/index.md",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "description", type: "string" },
+            { name: "layout", type: "string" },
+            { name: "emoji", type: "string" },
+            { name: "map", type: "list" },
+            { name: "weight", type: "number" },
+            {
+              name: "blocks",
+              type: "object",
+              fields: [
+                { name: "name", type: "string" },
+                { name: "src", type: "string" },
+              ],
+            },
+          ],
         },
       ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "static",
+        uploadDir: "uploaded",
+        publicPath: "/uploaded/",
+      },
     }),
   ],
-  // other properties...
 });
