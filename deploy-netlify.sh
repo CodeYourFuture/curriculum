35369456ec93783be60e4d558482bc70198a3730
Daniel Wagner-Hall <dawagner@gmail.com>
2024-12-06 11:15:28 +0000

    Share non-caching config across sites
    Previously some sites had config and others didn't, and none of them
    disabled getremote caching for production.
    
    This meant that things like PR lists may be stale, subject to hugo's
    default getremote caching. It also meant that changing this was a bit
    whack-a-mole across the sites.
    
    This changes every site to symlink in the same config (so we can
    centrally edit all sites), to disable caching for getremote in
    production, and to actually use the production config when deploying.

--- deploy-netlify.sh
+++ deploy-netlify.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # We set --baseURL on netlify so that any references to .Permalink end up pointing at deploy preview pages, rather than being hard-coded to point at the production URLs.
-hugo --minify --baseURL $DEPLOY_PRIME_URL && npx pagefind --site "public"
+hugo --minify --environment production --baseURL $DEPLOY_PRIME_URL && npx pagefind --site "public"
